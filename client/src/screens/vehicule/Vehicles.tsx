a38d27d7b5cb245d7aa1b19299e033fec5bbb18b
CristiCeban <cebancristi4444@gmail.com>
2020-12-26 17:43:07 +0200

    cors disabled,api client done

--- client/src/screens/vehicule/Vehicles.tsx
+++ client/src/screens/vehicule/Vehicles.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+import ApiService from "../../services/api";
+// import MaterialTable from "material-table";
+
+export interface VehicleInterface {
+    nrVehicol : number,
+    marca : string,
+    tip : string,
+    serieMotor : string,
+    serieCaroserie : string,
+    carburant : string,
+    culoare : string,
+    cappacitateCil : number,
+}
 
 const Vehicles = () => {
+
+    const [data, setData] = useState<VehicleInterface[]>([]); //table data
+    const [inProgress,setInProgress] = useState<boolean>(true)
+    const [iserror, setIserror] = useState<boolean>(false)
+    const [errorMessages, setErrorMessages] = useState<string[]>([])
+
+    useEffect(() => {
+        (async () => {
+           try{
+               setInProgress(true)
+               const response = await ApiService.get('vehicol', {})
+               setData(response);
+           }
+           catch (e) {
+               console.log(e);
+               setIserror(true);
+               setErrorMessages(['Server Error'])
+           }
+           finally {
+               setInProgress(false);
+           }
+        })()
+    },[])
     return(
         <h1>Vehicles</h1>
     )
